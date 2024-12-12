b4a5f74a26d24bf0b0a66dc1e9f795ffda93d1ef
anismahenthra <anismahenthra@gmail.com>
2024-12-12 20:09:39 +0530

    Update carousel.js

--- carousel.js
+++ carousel.js
@@ -1,65 +1,48 @@
-// Image URLs for the 1st and 2nd images
-const image1 = "https://static.wixstatic.com/media/3e231f_c8bf5858d17643c9a7d2bfdec1149d29~mv2.png";
-const image2 = "https://static.wixstatic.com/media/3e231f_646dda40800d48a39ef124cf9fb2fbb8~mv2.png";
+let currentImageIndex = 0;
+const images = [
+    "https://static.wixstatic.com/media/3e231f_c8bf5858d17643c9a7d2bfdec1149d29~mv2.png",
+    "https://static.wixstatic.com/media/3e231f_646dda40800d48a39ef124cf9fb2fbb8~mv2.png",
+    "https://static.wixstatic.com/media/3e231f_c8bf5858d17643c9a7d2bfdec1149d29~mv2.png",
+    "https://static.wixstatic.com/media/3e231f_646dda40800d48a39ef124cf9fb2fbb8~mv2.png",
+    "https://static.wixstatic.com/media/3e231f_c8bf5858d17643c9a7d2bfdec1149d29~mv2.png",
+    "https://static.wixstatic.com/media/3e231f_646dda40800d48a39ef124cf9fb2fbb8~mv2.png"
+];
 
-// Create an array of 20 images, alternating between image1 and image2
-const images = [];
-for (let i = 0; i < 20; i++) {
-    if (i % 2 === 0) {
-        images.push(image1);  // Add image1 for even indexes
-    } else {
-        images.push(image2);  // Add image2 for odd indexes
-    }
-}
-
-let currentIndex = 0;
 const mainImage = document.getElementById("mainImage");
+const thumbnails = document.getElementById("thumbnails");
 const prevButton = document.getElementById("prevButton");
 const nextButton = document.getElementById("nextButton");
-const thumbnailsContainer = document.querySelector('.thumbnails');
 
-/* Update the main image based on the current index */
-function updateMainImage() {
-    mainImage.src = images[currentIndex];
+function changeImage(imageSrc) {
+    mainImage.src = imageSrc;
 }
 
-/* Event listener for "prev" button */
-prevButton.addEventListener("click", () => {
-    currentIndex = (currentIndex === 0) ? images.length - 1 : currentIndex - 1;
-    updateMainImage();
-});
-
-/* Event listener for "next" button */
-nextButton.addEventListener("click", () => {
-    currentIndex = (currentIndex === images.length - 1) ? 0 : currentIndex + 1;
-    updateMainImage();
-});
-
-/* Function to change the main image when a thumbnail is clicked */
-function changeImage(imageUrl) {
-    mainImage.src = imageUrl;
+function showNextImage() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    mainImage.src = images[currentImageIndex];
 }
 
-/* Scroll thumbnails horizontally */
-let scrollAmount = 0;
-const thumbnailWidth = 210; // Thumbnail width with margin
+function showPrevImage() {
+    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+    mainImage.src = images[currentImageIndex];
+}
 
-/* Scroll thumbnails left */
-function scrollLeft() {
-    if (scrollAmount > 0) {
-        scrollAmount -= thumbnailWidth * 5; // Scroll by 5 thumbnails
-        thumbnailsContainer.style.transform = `translateX(-${scrollAmount}px)`;
+function scrollThumbnails(direction) {
+    const thumbnailsContainer = document.querySelector('.thumbnails-container');
+    const scrollAmount = 200;
+    if (direction === 'next') {
+        thumbnailsContainer.scrollBy(scrollAmount, 0);
+    } else if (direction === 'prev') {
+        thumbnailsContainer.scrollBy(-scrollAmount, 0);
     }
 }
 
-/* Scroll thumbnails right */
-function scrollRight() {
-    if (scrollAmount < thumbnailsContainer.scrollWidth - thumbnailsContainer.clientWidth) {
-        scrollAmount += thumbnailWidth * 5; // Scroll by 5 thumbnails
-        thumbnailsContainer.style.transform = `translateX(-${scrollAmount}px)`;
-    }
-}
+prevButton.addEventListener("click", function() {
+    showPrevImage();
+    scrollThumbnails('prev');
+});
 
-// Button event listeners for scrolling thumbnails
-document.getElementById('prevButton').addEventListener("click", scrollLeft);
-document.getElementById('nextButton').addEventListener("click", scrollRight);
+nextButton.addEventListener("click", function() {
+    showNextImage();
+    scrollThumbnails('next');
+});
